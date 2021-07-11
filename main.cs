c068947f320256cfea7150d616f33c10a6b70829
shahoreer <81192173+shahoreer@users.noreply.github.com>
2021-07-10 20:59:48 -0400

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,23 @@
+// find the smallest duplicate number in an arry. c#
+using System;
+class HelloWorld {
+  static void Main() {
+    int[] allnums={4,3,5,6,1};
+      
+    if(smallestduplicate(allnums) != -1)
+        Console.WriteLine(smallestduplicate(allnums));
+    else
+        Console.WriteLine("no duplicate number");
+  }
+  
+  static int smallestduplicate(int[] nums){
+      int sd=-1;
+      for(int i=0;i<nums.Length;i++){
+          for(int j=0;j<nums.Length;j++){
+              if(nums[i]==nums[j] && i<j && (sd > nums[i] || sd==-1))
+              sd=nums[i];
+          }
+      }
+      return sd;
+  }
+}
